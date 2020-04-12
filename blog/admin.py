27424bb753ad2fa808b5a4693ddfa3fc41c133bd
maxiumalong <c11614016@stu.ahu.edu.cn>
2020-04-12 19:50:39 +0800

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+
+from django.contrib import admin
+
+from . import models
+
+# from jax import models
+# Register your models here.
+admin.site.register(models.Article)
---
+++ blog/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from . import models
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, reverse
+def index(request):
+    articles=models.Article.objects.all()
+    return render(request,'blog/index.html',{'articles':articles})
+
+def article_page(request,article_id):
+    article=models.Article.objects.get(pk=article_id)
+    return render(request,'blog/article_page.html',{'article':article})
+
+def edit_page(request,article_id):
+    if str(article_id)=='0':
+        return render(request,'blog/edit_page.html')
+    article = models.Article.objects.get(pk=article_id)
+    return render(request,'blog/edit_page.html',{'article':article})
+
+def edit_action(request):
+    title = request.POST.get('title', 'TITLE')
+    content = request.POST.get('content', 'CONTENT')
+    article_id = request.POST.get('article_id', 'ARTICLE_ID')
+    if (title != 'TITLE') and (article_id == '0'):
+        models.Article.objects.create(title=title, content=content)
+        articles = models.Article.objects.all()
+        return redirect(reverse('blog:index'), {'articles': articles})
+    elif article_id != 'ARTICLE_ID':
+        article = models.Article.objects.get(pk=article_id)
+        article.title = title
+        article.content = content
+        article.save()
+        # 在这里重定向一下，reverse函数需要传进去文章的id
+        return redirect(reverse('blog:article_page', kwargs={'article_id': article_id}), {'article': article})
